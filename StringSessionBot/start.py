faa3153ba6dc5a3b09daab37ea12d658d0f23224
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-03 01:18:30 +0530

    Update start.py

--- StringSessionBot/start.py
+++ StringSessionBot/start.py
@@ -5,16 +5,20 @@ from telethon import events
 from pyrogram.types import InlineKeyboardMarkup
 from Config import ALIVE_PIC
 
-ALPHA_PIC = ALIVE_PIC if ALIVE_PIC else "https://te.legra.ph/file/9a207e6e453a93ab2b165.jpg"
+ALPHA_IMG = ALIVE_PIC if ALIVE_PIC else "https://te.legra.ph/file/9a207e6e453a93ab2b165.jpg"
 
 # Start Message
 @TelegramClient.on(events.NewMessage(pattern="/start"))
-async def start(bot, msg):
-	user = await bot.get_me()
-	mention = user["mention"]
-	await bot.send_message(
-		msg.chat.id,
-                ALPHA_PIC,
-		Data.START.format(msg.from_user.mention, mention),
-		reply_markup=InlineKeyboardMarkup(Data.buttons)
-	)
+async def start(event):              
+    if event.is_private:
+       MigBot = await event.client.get_me()
+       bot_name = MigBot.first_name
+       bot_id = MigBot.id
+       replied_user = await event.client(GetFullUserRequest(event.sender_id))
+       TheMighty = event.chat_id
+       firstname = replied_user.user.first_name
+       userid = replied_user.user.id
+       await event.client.send_file(TheMighty,
+                  ALPHA_IMG,
+                  caption=ownermsg, 
+                  buttons=Mig_Button)
