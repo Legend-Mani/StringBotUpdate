b316afc4f26999b26f1bb4761dff2533fafe31d3
Yashvi’s Alpha <101687704+theend-alpha@users.noreply.github.com>
2022-05-03 00:19:08 +0530

    Update start.py

--- StringSessionBot/start.py
+++ StringSessionBot/start.py
@@ -10,7 +10,7 @@ ALPHA_PIC = ALIVE_PIC if ALIVE_PIC else "https://te.legra.ph/file/9a207e6e453a93
 async def start(bot, msg):
 	user = await bot.get_me()
 	mention = user["mention"]
-	await bot.send_message(
+	await bot.send_file(ALPHA_PIC,
 		msg.chat.id,
 		Data.START.format(msg.from_user.mention, mention),
 		reply_markup=InlineKeyboardMarkup(Data.buttons)
